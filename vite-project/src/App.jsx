88afd8f53d65823e33d4b6bc6b65449350cb95b0
Maria Luiza <maria.silva5315@etec.sp.gov.br>
2025-04-10 00:11:13 -0300

    principio de componentes

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,13 @@
+import {Cabecalho} from './componentes/cabecalho/cabecalho';
 import './App.css'
 
 const App = () => {
   return (
+    <>
     <h1>Hello World</h1>
+    <Cabecalho/>
+    </>
   )
 }
 
-export default App
+export {App}
---
+++ vite-project/src/componentes/index.js
@@ -0,0 +1,1 @@
+import * as Cabecalho from './Cabecalho.js';
